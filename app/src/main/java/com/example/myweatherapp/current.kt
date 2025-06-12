0830085ead881fd7793618f612f5e2e7dc7e8c5f
Hans Fredrick <hansfredrick1590@gmail.com>
2025-06-12 19:07:36 +0800

    added weatherapi interface

---
+++ app/src/main/java/com/example/myweatherapp/current.kt
@@ -0,0 +1,6 @@
+package com.example.myweatherapp
+
+data class current(
+    val current: CurrentX,
+    val location: Location
+)
---
+++ app/src/main/java/com/example/myweatherapp/domain/api/WeatherApi.kt
@@ -0,0 +1,4 @@
+package com.example.myweatherapp.domain.api
+
+interface WeatherApi {
+}
