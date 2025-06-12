77072ad15a957e90791e34e2be3f66efbcc84a9a
Hans Fredrick <hansfredrick1590@gmail.com>
2025-06-12 18:28:30 +0800

    added bottom navigation

---
+++ app/src/main/java/com/example/myweatherapp/presentation/fragments/ContentFragment.kt
@@ -0,0 +1,7 @@
+package com.example.myweatherapp.presentation.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myweatherapp.R
+
+class ContentFragment : Fragment(R.layout.fragment_content) {
+}
